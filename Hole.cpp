eab3195ecd6f72c87d562e162dc5ca2923a445b1
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-19 13:18:55 -0500

    hey

--- Hole.cpp
+++ Hole.cpp
@@ -5,9 +5,9 @@
 typedef std::vector<double> vector;
 void initial_conditions(vector & data, int N);
 void cream_in_coffee(vector & data, int N,int lmin, int lmax,double DELTA);
-void evolve(vector & data, vector & prob, int Np, int N, int nsteps,double Xmin,double Ymin,double DELTA,int Npart,int u,int hole);
-void entropy(vector & data, int N, int b, double DELTA,int hole,int &Npart);
-double grid(vector & data, vector & prob, int Np, int N,int Npart);
+void evolve(vector & data, vector & prob, int Ng , int N, int nsteps,double Xmin,double Ymin,double DELTA,int Np,int u,int hole);
+void entropy(vector & data, int N, int b, double DELTA,int hole,int &Np);
+double grid(vector & data, vector & prob, int Ng , int N,int Np);
 void print_screen(const vector & data, int N);
 void start_gnuplot(double Xmin,double Ymin);
 void print_gnuplot(const vector & data, int N, double DELTA, double Xmin,double Ymin);
@@ -22,17 +22,18 @@ int main(int argc, char **argv)
 	double Ymin = -N/2;
 	int lmin = -Xmin-(Npart/2);
 	int lmax = -Xmin+(Npart/2);
-	int Np = N/10;
+	int Ng  = N/10;
+    int Np = Npart * Npart;
 	int hole = 10;
     // declare data structures
   vector potential(N*N); 
-	vector prob(Np*Np);
+	vector prob(Ng *Ng );
     // set initial and boundary conditions
   initial_conditions(potential, N);
-	initial_conditions(prob, Np);
+	initial_conditions(prob, Ng );
   cream_in_coffee(potential, N, lmin,lmax, DELTA);
     // evolve and print
-  evolve(potential,prob,Np, N, NSTEPS, Xmin, Ymin, DELTA,Npart,u, hole);
+  evolve(potential,prob,Ng , N, NSTEPS, Xmin, Ymin, DELTA,Np,u, hole);
   return 0;
 }
 void initial_conditions(vector & data, int N)
@@ -51,15 +52,15 @@ void cream_in_coffee(vector & data, int N,int lmin, int lmax,double DELTA)
         }
     }  
 }
-void evolve(vector & data, vector & prob, int Np, int N, int nsteps,double Xmin,double Ymin,double DELTA,int Npart,int u,int hole)
+void evolve(vector & data, vector & prob, int Ng , int N, int nsteps,double Xmin,double Ymin,double DELTA,int Np,int u,int hole)
 {
 	if (u == 0){  //Create a Gif using Gnuplot
     start_gnuplot(Xmin,Ymin);
     print_gnuplot(data, N, DELTA,Xmin,Ymin);
     for(int istep = 0; istep < nsteps; istep += 1) {
-        entropy(data, N,istep, DELTA, hole,Npart);
+        entropy(data, N,istep, DELTA, hole,Np);
 	if (istep%100 == 0) {
-		if (Npart == 0) {
+		if (Np == 0) {
 			exit (EXIT_FAILURE);
 		}
         print_gnuplot(data, N, DELTA,Xmin,Ymin);
@@ -68,33 +69,33 @@ void evolve(vector & data, vector & prob, int Np, int N, int nsteps,double Xmin,
 	}
 	if (u == 1){  //Print the entropy
     //print_screen(data, N);
-    std::cout << 0 << "\t" << grid(data,prob,Np, N, Npart) << std::endl;
-    initial_conditions(prob, Np);
+    std::cout << 0 << "\t" << grid(data,prob,Ng , N, Np) << std::endl;
+    initial_conditions(prob, Ng );
     for(int istep = 1; istep <= nsteps; istep += 1) {
-        entropy(data, N,istep, DELTA, hole,Npart);
+        entropy(data, N,istep, DELTA, hole,Np);
         //print_screen(data, N);
-	if (Npart == 0) {
+	if (Np == 0) {
 		exit (EXIT_FAILURE);
 	}
         if (istep%100 == 0) {
-		  	std::cout << istep << "\t" << grid(data,prob,Np, N, Npart) << std::endl;
-		  	initial_conditions(prob, Np);
+		  	std::cout << istep << "\t" << grid(data,prob,Ng , N, Np) << std::endl;
+		  	initial_conditions(prob, Ng );
     		}
 			}
 		}
 	else{
 		
-	std::cout << 0 << "\t" << Npart << std::endl;
+	std::cout << 0 << "\t" << Np << std::endl;
     	for(int istep = 1; istep <= nsteps; istep += 1) {
-        entropy(data, N,istep, DELTA, hole,Npart);
-	if (Npart == 0) {
+        entropy(data, N,istep, DELTA, hole,Np);
+	if (Np == 0) {
 		exit (EXIT_FAILURE);
 	}
-	std::cout << istep << "\t" <<  Npart << std::endl;
+	std::cout << istep << "\t" <<  Np << std::endl;
 	}	
 	}
 }
-void entropy(vector & data, int N,int b, double DELTA,int hole,int &Npart){
+void entropy(vector & data, int N,int b, double DELTA,int hole,int &Np){
     std::mt19937 gen(b);
     std::uniform_real_distribution<double> dis(0, 4.0);
     for(int ix = 0; ix < N; ++ix) {
@@ -145,7 +146,7 @@ void entropy(vector & data, int N,int b, double DELTA,int hole,int &Npart){
                     if(ix == N-1){
 			if (N/2 - hole/2 <= iy <= N/2 + hole/2){
 				data[ix*N + iy] = 0.0;
-				Npart -= 1;
+				Np -= 1;
 				}
 			else{
                         continue;
@@ -166,19 +167,19 @@ void entropy(vector & data, int N,int b, double DELTA,int hole,int &Npart){
         }
     }
 }
-double grid(vector & data, vector & prob, int Np, int N,int Npart){
-	for(int ii = 0; ii < Np; ++ii) {
-		for(int jj = 0; jj < Np; ++jj) {
-		    for(int ix = (N/Np)*ii; ix < (N/Np)*(ii+1); ++ix) {
-                for(int iy = (N/Np)*jj; iy < (N/Np)*(jj+1); ++iy) {
-                    prob[ii*Np+jj] += data[ix*N + iy]; 
+double grid(vector & data, vector & prob, int Ng , int N,int Np){
+	for(int ii = 0; ii < Ng ; ++ii) {
+		for(int jj = 0; jj < Ng ; ++jj) {
+		    for(int ix = (N/Ng )*ii; ix < (N/Ng )*(ii+1); ++ix) {
+                for(int iy = (N/Ng )*jj; iy < (N/Ng )*(jj+1); ++iy) {
+                    prob[ii*Ng +jj] += data[ix*N + iy]; 
                 }
 			}
-      prob[ii*Np+jj] = prob[ii*Np+jj]/(Npart*Npart);
+      prob[ii*Ng +jj] = prob[ii*Ng +jj]/(Np);
          }    
 	} 
     double total = 0;
-	for(int kk=0; kk< Np*Np; ++kk){
+	for(int kk=0; kk< Ng*Ng; ++kk){
 		if (prob[kk] != 0){
 		total +=  prob[kk] * - std::log(prob[kk]) ; 
 		}
