65848ea1f528e697679089698a1ccf56ef5bc053
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-18 19:33:02 -0500

    oh

--- Proy_Final.cpp
+++ Proy_Final.cpp
@@ -3,41 +3,43 @@
 #include <random>
 #include <cmath>
 
-const double DELTA = 1.0;
-const int N =  350;
-const int Npart =  60; //N particles = 60*60
-const int lmin = (N/2)-(Npart/2);
-const int lmax = (N/2)+(Npart/2);
-const int NSTEPS = 10000;
-const int Np = N/10;
-
 typedef std::vector<double> vector;
 
 void initial_conditions(vector & data, int N);
-void cream_in_coffee(vector & data, int N);
-void evolve(vector & data, vector & prob, int Np, int N, int nsteps);
-void entropy(vector & data, int N);
-double grid(vector & data, vector & prob, int Np, int N);
+void cream_in_coffee(vector & data, int N,int lmin, int lmax,double DELTA);
+void evolve(vector & data, vector & prob, int Np, int N, int nsteps,double Xmin,double Ymin,double DELTA,int Npart,int u);
+void entropy(vector & data, int N, int b, double DELTA);
+double grid(vector & data, vector & prob, int Np, int N,int Npart);
 void print_screen(const vector & data, int N);
-void start_gnuplot(void);
-void print_gnuplot(const vector & data, int N);
+void start_gnuplot(double Xmin,double Ymin);
+void print_gnuplot(const vector & data, int N, double DELTA, double Xmin,double Ymin);
 
 
 int main(int argc, char **argv)
 {
+	int N = std::atoi(argv[1]); // Matrix  (500)
+	int Npart = std::atoi(argv[2]); // Particles = Npart*Npart (20)
+	int NSTEPS = std::atoi(argv[3]); //Steps
+	int u = std::atoi(argv[4]); //Steps
+
+	double DELTA = 1.0;
+	double Xmin = -N/2;
+	double Ymin = -N/2;
+	int lmin = -Xmin-(Npart/2);
+	int lmax = -Xmin+(Npart/2);
+	int Np = N/10;
     // declare data structures
-        vector potential(N*N); // [ii, jj] -> ii* + jj
-		vector prob(Np*Np);
-		//int Np = std::atoi(argv[1]);
+  vector potential(N*N); 
+	vector prob(Np*Np);
     // set initial and boundary conditions
-    initial_conditions(potential, N);
-		initial_conditions(prob, Np);
-    cream_in_coffee(potential, N);
+  initial_conditions(potential, N);
+	initial_conditions(prob, Np);
+  cream_in_coffee(potential, N, lmin,lmax, DELTA);
 
     // evolve and print
-    evolve(potential,prob,Np, N, NSTEPS);
+  evolve(potential,prob,Np, N, NSTEPS, Xmin, Ymin, DELTA,Npart,u);
 
-    return 0;
+  return 0;
 }
 
 void initial_conditions(vector & data, int N)
@@ -48,34 +50,45 @@ void initial_conditions(vector & data, int N)
         }
     }
 }
-void cream_in_coffee(vector & data, int N)
+void cream_in_coffee(vector & data, int N,int lmin, int lmax,double DELTA)
 {
     for(int ix = lmin; ix < lmax; ++ix) {
         for(int iy = lmin; iy < lmax; ++iy) {
-        data[ix*N + iy] = 1.0;
+        data[ix*N + iy] = DELTA;
         }
     }  
 }
 
-void evolve(vector & data, vector & prob, int Np, int N, int nsteps)
+void evolve(vector & data, vector & prob, int Np, int N, int nsteps,double Xmin,double Ymin,double DELTA,int Npart,int u)
 {
-    start_gnuplot();
-    print_gnuplot(data, N);
-    //print_screen(data, N);
-    //grid(data,prob,Np, N);
-    initial_conditions(prob, Np);
+	if (u == 0){  //Create a Gif using Gnuplot
+    start_gnuplot(Xmin,Ymin);
+    print_gnuplot(data, N, DELTA,Xmin,Ymin);
+
     for(int istep = 0; istep < nsteps; istep += 1) {
-        entropy(data, N);
-        //print_screen(data, N);
-		//std::cout << istep << "\t" << grid(data,prob,Np, N) << std::endl;
-        //initial_conditions(prob, Np);
-        print_gnuplot(data, N);
+        entropy(data, N,71, DELTA);
+				if (istep%100 == 0) {
+        print_gnuplot(data, N, DELTA,Xmin,Ymin);
+				}
     }
+	}
+	else{  //Print the entropy
+    //print_screen(data, N);
+    std::cout << 0 << "\t" << grid(data,prob,Np, N, Npart) << std::endl;
+    initial_conditions(prob, Np);
+    for(int istep = 1; istep <= nsteps; istep += 1) {
+        entropy(data, N,istep, DELTA);
+        //print_screen(data, N);
+        if (istep%100 == 0) {
+		  	std::cout << istep << "\t" << grid(data,prob,Np, N, Npart) << std::endl;
+		  	initial_conditions(prob, Np);
+    		}
+			}
+		}
 }
 
-void entropy(vector & data, int N)
-{
-    std::mt19937 gen(5);
+void entropy(vector & data, int N,int b, double DELTA){
+    std::mt19937 gen(b);
     std::uniform_real_distribution<double> dis(0, 4.0);
     for(int ix = 0; ix < N; ++ix) {
         for(int iy = 0; iy < N; ++iy) {
@@ -89,9 +102,9 @@ void entropy(vector & data, int N)
                             //data[(ix+1)*N + iy] = 1.0;
                         }
                     else if (data[(ix-1)*N + iy] == 0.0){
-                            data[ix*N + iy] = 0.0;
-                            data[(ix-1)*N + iy] = 1.0;
-                    }
+                        data[ix*N + iy] = 0.0;
+                        data[(ix-1)*N + iy] = DELTA;
+                    } 
                     else {
                         continue;
                     }
@@ -105,7 +118,7 @@ void entropy(vector & data, int N)
                         }
                     else if (data[ix*N + iy+1] == 0.0){
                             data[ix*N + iy] = 0.0;
-                            data[ix*N + iy+1] = 1.0;
+                            data[ix*N + iy+1] = DELTA;
                     } 
                     else {
                         continue;
@@ -120,7 +133,7 @@ void entropy(vector & data, int N)
                         }
                     else if (data[ix*N + iy-1] == 0.0){
                             data[ix*N + iy] = 0.0;
-                            data[ix*N + iy-1] = 1.0;
+                            data[ix*N + iy-1] = DELTA;
                     } 
                     else {
                         continue;
@@ -135,7 +148,7 @@ void entropy(vector & data, int N)
                         }
                     else if (data[(ix+1)*N + iy] == 0.0){
                         data[ix*N + iy] = 0.0;
-                        data[(ix+1)*N + iy] = 1.0;
+                        data[(ix+1)*N + iy] = DELTA;
                     } 
                     else {
                         continue;
@@ -150,8 +163,7 @@ void entropy(vector & data, int N)
 
 }
 
-double grid(vector & data, vector & prob, int Np, int N)
-{
+double grid(vector & data, vector & prob, int Np, int N,int Npart){
 	for(int ii = 0; ii < Np; ++ii) {
 		for(int jj = 0; jj < Np; ++jj) {
 		    for(int ix = (N/Np)*ii; ix < (N/Np)*(ii+1); ++ix) {
@@ -176,8 +188,7 @@ double grid(vector & data, vector & prob, int Np, int N)
 }
 
 
-void print_screen(const vector & data, int N)
-{
+void print_screen(const vector & data, int N){
     for(int ix = 0; ix < N; ++ix) {
         for(int iy = 0; iy < N; ++iy) {
             std::cout << data[ix*N + iy] << "  ";
@@ -187,24 +198,23 @@ void print_screen(const vector & data, int N)
     std::cout << "\n";
 }
 
-void start_gnuplot(void)
+void start_gnuplot(double Xmin,double Ymin)
 {
     std::cout << "set term gif animate delay 10\n";
-    std::cout << "set output 'anim_03.gif'\n";
-		std::cout << "set xrange [0.0:"<<N<<".0]\n";
+    std::cout << "set output 'Entropy_istep.gif'\n";
+		std::cout << "set xrange ["<<Xmin<<":"<<-Xmin<<".0]\n";
 
-		std::cout << "set yrange [0.0:"<<N<<".0]\n";
-		
+		std::cout << "set yrange ["<<Ymin<<":"<<-Ymin<<".0]\n";
 }
 
-void print_gnuplot(const vector & data, int N)
+void print_gnuplot(const vector & data, int N, double DELTA, double Xmin,double Ymin)
 {
-    std::cout << "plot '-' w p ls 2 \n";
+    std::cout << "plot '-' w p ls 3 \n";
     for(int ix = 0; ix < N; ++ix) {
-        double x =  ix*DELTA;
+        double x =  Xmin + ix*DELTA;
         for(int iy = 0; iy < N; ++iy) {
 					if (data[ix*N + iy] != 0.0){
-            double y = iy*DELTA;
+            double y = Ymin + iy*DELTA;
             std::cout << x << "\t" << y << "\n";
         }
 				else{
