e3c8ba3fb6425fd6974122c173f0a764eff416ac
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-19 13:39:53 -0500

    aa

--- Hole.cpp
+++ Hole.cpp
@@ -62,7 +62,7 @@ void evolve(vector & data, vector & prob, int Ng , int N, int nsteps,double Xmin
 	if (Np == 0) {
 			exit (EXIT_FAILURE);
 		}
-	if (istep <= 17060){
+	if (istep <= 16000){
         if (istep%100 == 0) {
 		   print_gnuplot(data, N, DELTA,Xmin,Ymin);
     		}
