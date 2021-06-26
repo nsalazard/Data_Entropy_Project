ee0dabad1da7f1a3f7c8fb2100e00c39e551a9a4
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-25 19:37:20 -0500

    //

--- Hole.cpp
+++ Hole.cpp
@@ -108,16 +108,21 @@ void entropy(vector & data, int N,int b, double DELTA,int hole,int &Np){
         for(int iy = 0; iy < N; ++iy) {
             if (data[ix*N + iy] != 0.0){
                 double a = dis(gen);
-                //Move up
-                if (a < 1.0){
-                    if(ix == 0){
-			if (N/2-hole/2 <= iy && iy <= N/2+hole/2){
+		    
+		    if (ix == 0 && N/2-hole/2 <= iy && iy <= N/2+hole/2){
 				data[ix*N + iy] = 0.0;
 				Np -= 1;
 			}
-			else{
+                //Move up
+                if (a < 1.0){
+                    if(ix == 0){
+			//if (N/2-hole/2 <= iy && iy <= N/2+hole/2){
+				//data[ix*N + iy] = 0.0;
+				//Np -= 1;
+			//}
+			//else{
                         continue;
-			}
+			//}
                         }
                     else if (data[(ix-1)*N + iy] == 0.0){
                         data[ix*N + iy] = 0.0;
