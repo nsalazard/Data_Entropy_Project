db2d07768ee5ad45d7202e2c8152aabb76e03c19
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-07-21 18:22:01 -0500

    dd

--- Prot.cpp
+++ Prot.cpp
@@ -107,7 +107,6 @@ void evolve(vector &data, vector &prob, int Ng, int N, int nsteps, double Xmin,
    else if (u == 4) { // Test the entropy
     vector test(4);
     test[0] = grid(data, prob, Ng, N, Np);
-    std::cout << 0 << "\t" << E_0 << std::endl;
     initial_conditions(prob, Ng);
     for (int istep = 1; istep <= 1000; istep += 1) {
       entropy(data, N, istep, DELTA);
@@ -132,6 +131,7 @@ void evolve(vector &data, vector &prob, int Ng, int N, int nsteps, double Xmin,
         std::cout << "Something has gone wrong, please check it" << std::endl;
       }
     }
+   }
 }
 
 void entropy(vector &data, int N, int b, double DELTA) {
